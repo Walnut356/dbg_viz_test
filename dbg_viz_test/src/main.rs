dfd3119cb04d15e24eac64b780dd1eca67eb41d5
Walnut <39544927+Walnut356@users.noreply.github.com>
2025-05-29 04:05:28 -0500

    baseline working

--- dbg_viz_test/src/main.rs
+++ dbg_viz_test/src/main.rs
@@ -1,69 +1,32 @@
 #![allow(clippy::unused_io_amount)]
-
-// use std::{io::{self, pipe, BufRead, BufReader, Read, Write}, process::{Command, Stdio}, thread};
-
-// fn main() {
-//     let (child_read, mut child_write) = pipe().unwrap();
-//     let (mut parent_read, parent_write) = pipe().unwrap();
-
-//     let mut child = Command::new("lldb")
-//         .arg(r"C:\Users\ant_b\Documents\Coding Projects\dbg_viz_test\target\debug\sample.exe")
-//         .stdin(Stdio::piped())
-//         .stdout(Stdio::piped())
-//         .spawn()
-//         .unwrap();
-
-//     // Create threads for reading stdout and stderr
-//     let stdout_thread = thread::spawn(move || {
-//         let stdout = child.stdout.as_mut().expect("Failed to capture stdout");
-//         let stdout_reader = BufReader::new(stdout);
-//         for line in stdout_reader.lines() {
-//             if let Ok(line) = line {
-//                 println!("{}", line);
-//             }
-//         }
-//     });
-
-//     // Wait for both threads to finish
-//     stdout_thread.join().expect("stdout thread panicked");
-
-//     // let mut line = String::new();
-
-//     // parent_read.read_to_string(&mut line).unwrap();
-
-//     // println!("{line}");
-
-//     // while child.try_wait().is_ok_and(|x| x.is_none()) {
-
-//     //     if let Some(stdout) = child.stdout.as_mut() {
-//     //         let x = stdout.read_to_string(&mut line).unwrap();
-//     //         println!("read: {line}");
-//     //     }
-//     //     println!("read: {line}");
-//     // }
-// }
-
 use std::{process::Stdio, thread, time::Duration};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter};
 use tokio::process::Command;
 
+const LLDB_LOOKUP: &str = r"C:\Users\ant_b\.rustup\toolchains\stable-x86_64-pc-windows-msvc\lib\rustlib\etc\lldb_lookup.py";
+const LLDB_COMMANDS: &str = r"C:\Users\ant_b\.rustup\toolchains\stable-x86_64-pc-windows-msvc\lib\rustlib\etc\lldb_commands";
+
 #[derive(Debug, Clone, Copy)]
 enum State {
     Launch,
+    Ready,
     Running,
-    Breakpoint,
     VarCheck(u32),
 }
 
 /// Pairs of values for testing. The first value in the pair is the command to make lldb print the
 /// variable, the second is the output to check against. The output may span multiple lines.
-const VARS: &[(&str, &str)] = &[("v u8_v", "(unsigned char) ")];
+const CASES: &[&str] = &[
+    r"(unsigned char) u8_v = 0",
+    r"(unsigned short) u16_v = 1",
+    r"(unsigned int) u32_v = 2",
+];
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::new("lldb");
 
-    cmd.arg(r"C:\Users\ant_b\Documents\Coding Projects\dbg_viz_test\target\debug\sample.exe");
+    cmd.arg(r"./target\debug\sample.exe");
 
     // Specify that we want the command's standard output piped back to us.
     // By default, standard input/output/error will be inherited from the
@@ -96,12 +59,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut state = State::Launch;
 
-    let mut running = false;
-    let mut stopped = false;
-    let mut vars = false;
-    let mut var_count = 0;
     let mut line = String::new();
     loop {
+        line.clear();
         reader.read_line(&mut line).await?;
         print!("{line}");
 
@@ -109,53 +69,68 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             State::Launch => {
                 // ~indicates that lldb has parsed the debug info and is ready to accept commands
                 if line.starts_with("Current executable") {
-                    // prevents noise that makes it much harder to 
+                    // prevents noise that makes it much harder to determine what line we're at
+                    // later
                     stdin
                         .write("setting set stop-line-count-before 0\n".as_bytes())
                         .await?;
                     stdin
                         .write("setting set stop-line-count-after 0\n".as_bytes())
                         .await?;
+                    // load rust's formatters
+                    stdin
+                        .write(format!("command script import \"{LLDB_LOOKUP}\"\n").as_bytes())
+                        .await?;
+                    stdin
+                        .write(format!("command source -s true \"{LLDB_COMMANDS}\"\n").as_bytes())
+                        .await?;
                     stdin.write("b main.rs:6\n".as_bytes()).await?;
-                }
-            },
-            State::Running => todo!(),
-            State::Breakpoint => todo!(),
-            State::VarCheck(_) => todo!(),
-        }
 
-        if line.starts_with("Breakpoint") {
-            stdin.write("run\n".as_bytes()).await?;
-            running = true;
-            thread::sleep(Duration::from_secs(1));
-        }
-        if line.contains("Process") && line.contains("stopped") {
-            stopped = true;
-        }
-        if stopped && running && line.starts_with("    f") {
-            reader.read_line(&mut line);
-            vars = true;
-            stdin.write("v u8_v\n".as_bytes()).await?;
-            // line.clear();
-            // reader.read_line(&mut line);
-            // assert_eq!(line, "(unsigned char) u8_v = 0");
-        }
-
-        if vars {
-            match var_count {
-                0 => {
-                    if line.contains("(lldb) v u8_v") {
-                        line.clear();
-                        reader.read_line(&mut line).await?;
-                        assert_eq!(line, "(unsigned char) u8_v = '\\0'\n");
-                        var_count += 1;
-                    }
+                    state = State::Ready;
                 }
-                1 => if line.contains("(lldb) v ") {},
-                _ => {}
+            }
+            State::Ready => {
+                if line.starts_with("Breakpoint") {
+                    stdin.write("run\n".as_bytes()).await?;
+                    state = State::Running;
+                }
+            }
+            State::Running => {
+                // corresponds to the line:
+                // "frame #0: 0x00007ff683421158 sample.exe`sample::main at main.rs:N"
+                if line.starts_with("    f") {
+                    stdin.write("v\n".as_bytes()).await?;
+
+                    // consume the line we just wrote
+                    reader.read_line(&mut line).await?;
+                    state = State::VarCheck(0);
+                }
+            }
+            State::VarCheck(idx) => {
+                let expected = CASES[idx as usize];
+                // check against the output, which can span multiple lines.
+
+                let mut lines_iter = expected.lines();
+
+                // we already have the first line, so we check that first, then do loops to read
+                // more lines only if necessary.
+                let e = lines_iter.next().unwrap();
+                assert_eq!(e.trim(), line.trim());
+
+                for e in lines_iter {
+                    line.clear();
+                    reader.read_line(&mut line).await?;
+                    assert_eq!(e.trim(), line.trim());
+                }
+
+                if (idx + 1) as usize >= CASES.len() {
+                    println!("\n\nALL TEST CASES OKAY");
+                    return Ok(());
+                }
+
+                state = State::VarCheck(idx + 1);
             }
         }
-        line.clear();
     }
 
     Ok(())
